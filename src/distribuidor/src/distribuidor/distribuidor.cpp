9cec8933757b85bb1c79faf60ea9de8ad6ed29a4
l0w3nz0 <l.vera.1277@gmail.com>
2024-11-06 17:02:26 -0300

    Update distribuidor.cpp

--- src/distribuidor/src/distribuidor/distribuidor.cpp
+++ src/distribuidor/src/distribuidor/distribuidor.cpp
@@ -1,2 +1,14 @@
 #include "distribuidor.h"
+void distribuidor(string mensaje) {
+    cout << "Distribuyendo mensaje: " << mensaje << endl;
 
+
+    // Simulamos que se guarda el mensaje en un archivo de resultados.
+    ofstream file("assets/resultados.txt", ios::app);
+    if (file) {
+        file << mensaje << endl;
+        cout << "Mensaje guardado en el archivo de resultados." << endl;
+    } else {
+        cerr << "ERROR: No se pudo abrir el archivo de resultados." << endl;
+    }
+}
