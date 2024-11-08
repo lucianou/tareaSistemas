a2b4619c38c54310349c624a9de3fb8b2776f301
l0w3nz0 <l.vera.1277@gmail.com>
2024-11-07 21:04:53 -0300

    Update main.cpp

--- src/planificador/src/main.cpp
+++ src/planificador/src/main.cpp
@@ -1,19 +1,24 @@
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <cstring>
-#include <unistd.h>
-#include <string>
-#include <fstream>
-#include <sstream>
 #include "planificador.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    loadEnv("../../.env");
+int main(int argc, char* argv[]) {
+    if (argc < 4) { // Ajusta según el número de argumentos que esperes
+        cerr << "ERROR: Parámetros insuficientes en la ejecución de planificador.\n";
+        return 1;
+    }
+
+    string procesos = argv[1];
+    int cantidadCores = stoi(argv[2]);
+    string resultados = argv[3];
+
+    // Ahora puedes usar 'procesos', 'cantidadCores' y 'resultados' en el código.
+    cout << "Ruta de PROCESOS: " << procesos << endl;
+    cout << "CANTIDAD_CORES: " << cantidadCores << endl;
+    cout << "Ruta de RESULTADOS: " << resultados << endl;
+
     cout << "-------- [Planificador de operaciones] --------\n\n";
-    planificador();
+    planificador(procesos, cantidadCores, resultados);
     cout << "-----------------------------------------------\n\n";
     return 0;
 }
