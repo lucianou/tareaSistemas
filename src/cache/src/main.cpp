0297da40fa378c25351d549393c6e1b04b6ecc96
l0w3nz0 <l.vera.1277@gmail.com>
2024-11-08 11:52:35 -0300

    Update main.cpp

--- src/cache/src/main.cpp
+++ src/cache/src/main.cpp
@@ -1,11 +1,12 @@
 #include "cache.h"
 
 int main(int argc, char *argv[]) {
-    if (argc < 4) {
-        cerr << "ERROR: No se proporcionaron suficientes argumentos." << endl;
+    if (argc < 2) { 
+        cerr << "ERROR: Parámetros insuficientes en la ejecución de planificador.\n";
         return 1;
     }
-
+    int memory_size = stoi(argv[1]);
+    iniciarCache(memory_size);
 
     return 0;
 }
