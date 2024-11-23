3e89fe4686c32de1a74290b85ca2d0ad16289500
l0w3nz0 <l.vera.1277@gmail.com>
2024-11-23 03:17:55 -0300

    Update planificador.h

--- src/planificador/src/planificador/planificador.h
+++ src/planificador/src/planificador/planificador.h
@@ -13,6 +13,5 @@ using namespace std;
 
 void planificador(const string& procesosPathStr, int cantidadCores, const string& resultadosPathStr);                                    // Función principal del planificador
 bool validarFormatoMensaje(const string& mensaje);      // Función para validar el formato del mensaje
-void loadEnv(const std::string& filename);              // Función para cargar las variables de entorno
 
 #endif
